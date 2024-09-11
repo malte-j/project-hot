bae105beac07d824d3bfa2474fabae7d320adc1d
Malte <hi@malts.me>
2024-09-11 13:11:44 +0200

    remove old api

--- web/prisma/migrations/20231218203619_token/migration.sql
+++
@@ -1,4 +0,0 @@
--- CreateTable
-CREATE TABLE "ApiToken" (
-    "token" TEXT NOT NULL PRIMARY KEY
-);
--- web/src/config.ts
+++
@@ -1,7 +0,0 @@
-import fs from "fs";
-
-export const PORT = parseInt(process.env.PORT || "3000", 10);
-export const HOST = process.env.HOST || "localhost";
-export const TEMP_DIR = "/tmp/project-hot/";
-
-fs.mkdirSync(TEMP_DIR, { recursive: true });
